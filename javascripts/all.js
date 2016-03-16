56cb70d88523883487caedac596b5c5cbc9b70d7
Karine Bialobroda <karine.bialobroda@gmail.com>
2016-03-16 19:15:25 +0100

    Automated commit at 2016-03-16 18:15:24 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -12106,16 +12106,18 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 
 $(document).ready(function() {
   // navbar transition jQuery script
-  $(".navbar").hide();
+
   $(window).scroll(function(e){
     if ($(this).scrollTop() > 0) {
-      $(".navbar").show();
-      // $(".navbar").css({
-      //   "background": "rgba(255, 255, 255, 1)"
-      // });
+
+      $(".navbar").css({
+        "background": "rgba(255, 255, 255, 1)"
+      });
     }
     else {
-      $(".navbar").hide();
+      $(".navbar").css({
+        "background": "rgba(255, 255, 255, 0)"
+      });
     }
   });
 });
