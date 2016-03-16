655862450d41011a515c964d578205dae4018afe
Karine Bialobroda <karine.bialobroda@gmail.com>
2016-03-16 19:15:03 +0100

    small fixes

--- source/javascripts/navbarServices.js
+++ source/javascripts/navbarServices.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
   // navbar transition jQuery script
-  $(".navbar").hide();
+
   $(window).scroll(function(e){
     if ($(this).scrollTop() > 0) {
-      $(".navbar").show();
-      // $(".navbar").css({
-      //   "background": "rgba(255, 255, 255, 1)"
-      // });
+
+      $(".navbar").css({
+        "background": "rgba(255, 255, 255, 1)"
+      });
     }
     else {
-      $(".navbar").hide();
+      $(".navbar").css({
+        "background": "rgba(255, 255, 255, 0)"
+      });
     }
   });
 });
